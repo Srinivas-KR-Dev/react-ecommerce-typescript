5c425a452fceb49b24289c752ea0039c5b23ecdf
Srinivas-KR-Dev <07kr.srinivas@gmail.com>
2025-11-13 17:25:03 +0530

    Replace anchor link with Link  component for order and tranking page

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import Header from '../components/Header';
 import './OrdersPage.css';
 
@@ -58,11 +59,11 @@ export function OrdersPage() {
                         </div>
 
                         <div className="product-actions">
-                        <a href="/tracking">
+                        <Link to="/tracking">
                             <button className="track-package-button button-secondary">
                             Track package
                             </button>
-                        </a>
+                        </Link>
                         </div>
 
                         <div className="product-image-container">
@@ -86,11 +87,11 @@ export function OrdersPage() {
                         </div>
 
                         <div className="product-actions">
-                        <a href="/tracking">
+                        <Link to="/tracking">
                             <button className="track-package-button button-secondary">
                             Track package
                             </button>
-                        </a>
+                        </Link>
                         </div>
                     </div>
                     </div>
@@ -137,11 +138,11 @@ export function OrdersPage() {
                         </div>
 
                         <div className="product-actions">
-                        <a href="/tracking">
+                        <Link to="/tracking">
                             <button className="track-package-button button-secondary">
                             Track package
                             </button>
-                        </a>
+                        </Link>
                         </div>
                     </div>
                     </div>
--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import { Link } from 'react-router';
 import './TrackingPage.css';
 
 export function TrackingPage(){
@@ -8,13 +9,15 @@ export function TrackingPage(){
         <>
             <title>Tracking</title>
 
+            <link rel="icon" href="tracking-favicon.png" type="image/x-icon"/>
+
             <Header />
 
             <div className="tracking-page">
                 <div className="order-tracking">
-                    <a className="back-to-orders-link link-primary" href="/orders">
+                    <Link className="back-to-orders-link link-primary" to="/orders">
                     View all orders
-                    </a>
+                    </Link>
 
                     <div className="delivery-date">
                     Arriving on Monday, June 13
