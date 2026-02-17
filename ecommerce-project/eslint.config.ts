9199c1099b2ff4b5fd67dcd2c3880152f82ea21e
Srinivas-KR-Dev <07kr.srinivas@gmail.com>
2026-02-17 15:02:28 +0530

    Convert setup files to TS

--- ecommerce-project/eslint.config.ts
+++ ecommerce-project/eslint.config.ts
@@ -4,10 +4,13 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,4 +28,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+];
