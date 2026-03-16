b50be5a134421de174b0b99b4b6a4b5f51560fdd
Srinivas-KR-Dev <07kr.srinivas@gmail.com>
2026-03-16 17:16:18 +0530

    Chore: code cleanup

--- 07-ecommerce-project-js/src/utils/money.js
+++
@@ -1,7 +0,0 @@
-export function formatMoney(amountCents) {
-    if (amountCents < 0) {
-        return `-$${(Math.abs(amountCents) / 100).toFixed(2)}`
-    }
-
-    return `$${(amountCents / 100).toFixed(2)}`
-}
